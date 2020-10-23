dcf2f240c5afdaa65ba915cf7af43e41372a42b7
Diarit-S <diarit-94@hotmail.fr>
2020-10-23 14:48:24 +0200

    create shield

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    public float ShieldPower = 10;
+    public float ShieldUsingSpeed = 100;
+    float timer = 0;
+
+    void Start()
+    {
+        
+    }
+
+    public void RechargeShield() {
+        if (ShieldPower < 10)
+        {
+            ShieldPower++;
+        }
+    }
+
+    public void UseField() {
+         if (ShieldPower > 0)
+        {
+            timer ++;
+            if (timer == ShieldUsingSpeed)
+            {
+                ShieldPower--;
+                timer = 0;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("a key was pressed");
+            UseField();
+        }
+    }
+}
