6d0ce3308b37563126b37794bc1dd77f3d1f282b
MaksymY <maksym.yankivskyypro@gmail.com>
2020-10-23 11:37:36 +0200

    adding fire system

--- Assets/Scripts/MoveBullet.cs
+++ Assets/Scripts/MoveBullet.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveBullet : MonoBehaviour
+public class Move : MonoBehaviour
 {
     public Vector3 Direction;
     public float Speed;
+
+    /* private Global GlobalRef; */
+
+    // Start is called before the first frame update
+    /* void Start()
+    {
+        GlobalRef = GameObject.FindObjectOfType<Global>();
+    } */
 
     // Update is called once per frame
     void Update()
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerBullet : MonoBehaviour
+{
+    public GameObject ObjPrefabs;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GameObject spawnee = GameObject.Instantiate(ObjPrefabs);
+            Vector3 spawnerPos = transform.position;
+            spawnerPos.x += 0;
+            spawnee.transform.position = spawnerPos;
+        }
+    }
+}
