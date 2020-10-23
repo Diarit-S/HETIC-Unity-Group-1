5766481f3ce60840bd96f9641692f1e2c51cdd1e
MaksymY <maksym.yankivskyypro@gmail.com>
2020-10-23 14:28:20 +0200

    adding destruction block by bullet

--- Assets/Scripts/SpawnerBullet.cs
+++ Assets/Scripts/SpawnerBullet.cs
@@ -2,18 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Spawner : MonoBehaviour
+public class SpawnerBullet : MonoBehaviour
 {
     public GameObject ObjPrefabs;
+    public float bullets;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+        if (Input.GetKeyDown(KeyCode.Space) && bullets > 0) {
             GameObject spawnee = GameObject.Instantiate(ObjPrefabs);
             Vector3 spawnerPos = transform.position;
             spawnerPos.x += 0;
             spawnee.transform.position = spawnerPos;
+            bullets--;
         }
     }
 }
